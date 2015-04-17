4230a5f78cd2857602791535d96eb133b3a96d3a
Pal Mezei <sirpalee@gmail.com>
2015-04-17 22:11:06 +1000

    Test commit for an empty behavior.

---
+++ Assets/Code/SimpleBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
