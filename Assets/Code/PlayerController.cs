99a6021089d684e6cd73938a60df946a4ad08de3
Pal Mezei <sirpalee@gmail.com>
2015-04-18 18:10:36 +1000

    Some simple test commit. Just to test the initial stuff.

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator m_animator;
+    private Rigidbody2D m_rigidBody;
+
+    public float walkSpeed = 1.0f;
+    // Use this for initialization
+    void Start ()
+    {
+        m_animator = gameObject.GetComponentInChildren<Animator>();
+        m_rigidBody = gameObject.GetComponentInChildren<Rigidbody2D>();
+    }
+	
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        if (Camera.main == null) // ie the camera has not be done shit with
+            return;
+        Ray rayFromMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Vector3 groundPos = rayFromMouse.origin - rayFromMouse.direction * (rayFromMouse.origin.z / rayFromMouse.direction.z);      
+       
+
+        Vector3 aimingDirection = groundPos - transform.position;
+        transform.rotation = Quaternion.FromToRotation(new Vector3(0.0f, -1.0f, 0.0f), aimingDirection);
+
+        Vector3 speed = new Vector3(0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.W))
+            speed.y = Time.fixedDeltaTime;
+       else if (Input.GetKey(KeyCode.S))
+           speed.y = -Time.fixedDeltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+           speed.x = -Time.fixedDeltaTime;
+        else if (Input.GetKey(KeyCode.D))
+           speed.x = Time.fixedDeltaTime;
+
+        speed = speed * walkSpeed;
+
+        if (speed.sqrMagnitude > 0.001f)
+            m_animator.SetBool("walk", true);
+        else
+            m_animator.SetBool("walk", false);
+
+        transform.position = speed + transform.position;
+
+        // move the camera towards the player
+        Vector3 camPos = Camera.main.transform.position;
+    }
+}
