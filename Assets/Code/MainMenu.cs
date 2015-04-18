025593bb98214e5d3e8ab6a1dbb4696dcbf1ebf1
unknown <zk@Outpost.blackholearchives.org>
2015-04-19 00:06:53 +1000

    Adds additional scenes to govern overall game application layout.  Also includes test build and updated build parameters

---
+++ Assets/Code/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public void ChangeScene (int sceneInt) {
+		Application.LoadLevel (sceneInt);
+	}
+
+	public void ExitGame () {
+		Application.Quit ();
+	}
+}
+
+// EOF
---
+++ Assets/Code/Splash.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+	float timer	= 10;
+	
+	void Update () {
+		
+		timer -= Time.deltaTime;
+		
+		if (timer < 0) {
+			Application.LoadLevel("MainMenu");
+		}
+		
+		if (Input.anyKey && timer < 7) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+
+}
+
+// EOF
