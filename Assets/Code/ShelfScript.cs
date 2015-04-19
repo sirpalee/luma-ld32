5d8a88038200e0fe6d1cb17362151b5fe6e8d132
Pal Mezei <sirpalee@gmail.com>
2015-04-19 11:22:19 +1000

    Fixing search text initialisation, it's smoother and nicer.

--- Assets/Code/ShelfScript.cs
+++ Assets/Code/ShelfScript.cs
@@ -7,30 +7,18 @@ public class ShelfScript : MonoBehaviour {
     //[HideInInspector]
     public string containedItem = "None";
     public float searchTime = 5.0f;
+    public float searchableFromDistance = 2.5f;
 
-    private Canvas m_canvas;
+    private GameObject m_instancedUI;
     private RectTransform m_rect;
     private Coroutine m_barCoroutine;
 
     // Use this for initialization
-    void Start () {
-        m_canvas = GetComponentInChildren<Canvas>();
-        
-        if (m_canvas != null)
-            m_canvas.enabled = false;
-        else
-            throw new System.NotSupportedException();
-
-        Image image = GetComponentInChildren<Image>();
-
-        if (image == null)
-            throw new System.NotSupportedException();
-        else
-            m_rect = image.gameObject.GetComponent<RectTransform>();
-
-        m_rect.localScale = new Vector3(0.0f, 1.0f, 1.0f);
-
+    void Start ()
+    {
         m_barCoroutine = null;
+        m_rect = null;
+        m_instancedUI = null;
     }
 
     // Update is called once per frame
@@ -40,19 +28,20 @@ public class ShelfScript : MonoBehaviour {
 
     void OnMouseEnter()
     {
-        if (containedItem != "None")
-            m_canvas.enabled = true;
+        if ((containedItem != "") && 
+            (Vector3.Distance(transform.position, PlayerController.Instance.gameObject.transform.position)
+            < searchableFromDistance))
+        {
+            CreateText();
+        }
     }
 
     void OnMouseDown()
     {
-        if (m_barCoroutine == null)
+        if (m_barCoroutine == null && m_instancedUI != null)
         {
-            if (containedItem != "None")
-            {
-                m_rect.localScale = new Vector3(0.0f, 1.0f, 1.0f);
-                m_barCoroutine = StartCoroutine(AnimateBar());
-            }
+            m_rect.localScale = new Vector3(0.0f, 1.0f, 1.0f);
+            m_barCoroutine = StartCoroutine(AnimateBar());
         }
     }
     
@@ -64,7 +53,7 @@ public class ShelfScript : MonoBehaviour {
             m_rect.localScale = new Vector3(0.0f, 1.0f, 1.0f);
             m_barCoroutine = null;
         }
-        m_canvas.enabled = false;
+        else if (m_instancedUI != null) DeleteText();
     }
     
     
@@ -86,10 +75,50 @@ public class ShelfScript : MonoBehaviour {
             m_rect.localScale = Vector3.MoveTowards(m_rect.localScale, new Vector3(1.0f, 1.0f, 1.0f), Time.fixedDeltaTime);
             yield return null;
         }
-        // Give item to player
         m_barCoroutine = null;
         PlayerController.Instance.RecieveItem(containedItem);
-        containedItem = "None";
-        m_canvas.enabled = false;
+        containedItem = "";
+        DeleteText();
+    }
+
+    void CreateText()
+    {
+        if (m_instancedUI == null)
+        {
+            m_instancedUI = (GameObject)Object.Instantiate(Resources.Load("SearchMessage"), transform.position, transform.rotation);
+            Image image = m_instancedUI.GetComponentInChildren<Image>();
+            m_rect = image.gameObject.GetComponent<RectTransform>();
+            m_instancedUI.transform.SetParent(transform);
+            RectTransform parentRect = m_instancedUI.GetComponent<RectTransform>();
+            Vector3 dir = PlayerController.Instance.transform.position - transform.position;
+            bool dirX = Mathf.Abs(dir.x) > Mathf.Abs(dir.y);
+            Vector3 size = new Vector3(1.0f, 1.0f, 1.0f);
+            MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+            if (renderer != null)
+            {
+                Bounds bounds = renderer.bounds;
+                size.z = bounds.size.z / 2.0f;
+                size.x = bounds.size.x / 2.0f;
+                size.y = bounds.size.y / 2.0f;
+            }
+
+            // TODO use the size of the object to place the message at the right place
+            Vector3 offsetDir = new Vector3(0.0f, 0.0f, -size.z);
+            if (dirX)
+                offsetDir.x = Mathf.Sign(dir.x) * (size.x + 0.2f);
+            else
+                offsetDir.y = Mathf.Sign(dir.y) * (size.z + 0.2f);
+            parentRect.localPosition = parentRect.localPosition + offsetDir;
+            m_rect.localScale = new Vector3(0.0f, 1.0f, 1.0f);
+        }
+    }
+
+    void DeleteText()
+    {
+        if (m_instancedUI != null)
+        {
+            m_rect = null;
+            Object.Destroy(m_instancedUI);
+        }
     }
 }
