7b48513d61f85583195d2eda58781ed290f1db0f
unknown <zk@Outpost.blackholearchives.org>
2015-04-20 00:35:34 +1000

    In a none broken state

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 public class MainMenu : MonoBehaviour {
 
@@ -9,15 +12,53 @@ public class MainMenu : MonoBehaviour {
 	int musicTog;
 	int soundTog;
 
-	public void playSound ( int soundLibrary ) {
-		AudioSource temp = AudioSource.FindObjectOfType<AudioSource>();
+	String[] soundLibrary = new String[50];
 
-		if (temp != null) {
-			temp.Play();
+	public void soundLibUpdate(string path) {
+
+		//for (i=0; i<2; i++) {
+		//	audios[i] = Resources.Load(soundfiles[i]);
+		//}
+
+		DirectoryInfo info = new DirectoryInfo (Application.dataPath+"/"+path);
+		FileInfo[] fileEntries = info.GetFiles ();
+
+		int fileCount = 0;
+
+		foreach (FileInfo soundFile in fileEntries) {
+
+			Match match = Regex.Match( soundFile.FullName , @"\.meta$", RegexOptions.IgnoreCase);
+			if (match.Success)
+				continue;
+
+			if(soundFile.Name.ToString() != null )
+				soundLibrary[fileCount] = soundFile.Name;
+			fileCount++;
+
+			//match = Regex.Match( soundFile.FullName , @".*$", RegexOptions.IgnoreCase);
+			//string[] fileParts = Regex.Split(soundFile.FullName,'\\'+Path.DirectorySeparatorChar.ToString());
+			//foreach( string file in fileParts ) {
+			//	Debug.Log(file);
+			//}
+
+			// DEBUG
+			//Debug.Log(soundLibrary.Length);
+			//Debug.Log(soundFile.Name.ToString()  );
 		}
+		soundLibrary [10] = Regex.Replace (soundLibrary [10], @"\..*$", @"");
+		Debug.Log ("Sounds/"+soundLibrary[10]);
+
+
+		AudioClip sound = Resources.Load("Sounds/"+soundLibrary[10], typeof(AudioClip)) as AudioClip;
+		if (sound != null)
+			Debug.Log ("Here: "+sound.length);
+
+
+
+		// DEBUG
+		Debug.Log(fileCount);
 
 	}
-
 
 	public void ChangeScene (int sceneInt) {
 		Application.LoadLevel (sceneInt);
@@ -58,70 +99,40 @@ public class MainMenu : MonoBehaviour {
 	}
 
 	void Start () {
+		// Update Sound Library
+		this.soundLibUpdate ("Sounds");
+
+		// Zero the Listener while we figure out soundSettings
 		AudioListener.volume = 0;
 
-		if (PlayerPrefs.HasKey ("Music")) {
-			musicVol = PlayerPrefs.GetFloat("Music");
-		} else {
-			musicVol = 1;
-			PlayerPrefs.SetFloat("Music", musicVol);
-		}
-
 		if (PlayerPrefs.HasKey ("Sound")) {
-			soundVol = PlayerPrefs.GetFloat("Sound");
+			soundVol = PlayerPrefs.GetFloat ("Sound");
 		} else {
 			soundVol = 1;
-			PlayerPrefs.SetFloat("Sound", soundVol);
+			PlayerPrefs.SetFloat ("Sound", soundVol);
 		}
 
 		if (PlayerPrefs.HasKey ("SoundTog")) {
-			soundTog = PlayerPrefs.GetInt("SoundTog");
+			soundTog = PlayerPrefs.GetInt ("SoundTog");
 		} else {
 			soundTog = 1;
-			PlayerPrefs.SetInt("SoundTog", soundTog);
-		}
-
-		if (PlayerPrefs.HasKey ("MusicTog")) {
-			musicTog = PlayerPrefs.GetInt("MusicTog");
-		} else {
-			musicTog = 1;
-			PlayerPrefs.SetInt("MusicTog", musicTog);
+			PlayerPrefs.SetInt ("SoundTog", soundTog);
 		}
 
 		// Restore values in UI
 		GameObject temp = GameObject.Find ("SoundSlider");
 		if (temp != null) {
-			Slider tempSlide = temp.GetComponent<Slider>();
-			if(tempSlide != null) {
+			Slider tempSlide = temp.GetComponent<Slider> ();
+			if (tempSlide != null) {
 				tempSlide.value = soundVol;
-			}
-		}
-
-		temp = GameObject.Find ("MusicSlider");
-		if (temp != null) {
-			Slider tempSlide = temp.GetComponent<Slider>();
-			if(tempSlide != null) {
-				tempSlide.value = musicVol;
 			}
 		}
 
 		temp = GameObject.Find ("SoundToggle");
 		if (temp != null) {
-			Toggle tempTog = temp.GetComponent<Toggle>();
-			if(tempTog != null) {
-				if( soundTog > 0) {
-					tempTog.isOn = true;
-				} else {
-					tempTog.isOn = false;
-				}
-			}
-		}
-
-		temp = GameObject.Find ("MusicToggle");
-		if (temp != null) {
-			Toggle tempTog = temp.GetComponent<Toggle>();
-			if(tempTog != null) {
-				if( musicTog > 0) {
+			Toggle tempTog = temp.GetComponent<Toggle> ();
+			if (tempTog != null) {
+				if (soundTog > 0) {
 					tempTog.isOn = true;
 				} else {
 					tempTog.isOn = false;
