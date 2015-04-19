4166f132d157ea4fbfdf0f7579e0abadb45577f5
unknown <zk@Outpost.blackholearchives.org>
2015-04-19 21:23:14 +1000

    End of game changed

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -99,6 +99,8 @@ public class GameManager : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         if (Input.GetKey(KeyCode.Escape))
-            Application.LoadLevel("MainMenu");
+            Application.LoadLevel("GameOver");
     }
 }
+
+// EOF
