f5794d783328588c5a103144c68c477250474abb
Pal Mezei <sirpalee@gmail.com>
2015-04-19 12:54:43 +1000

    Exiting game when esc pressed. You're welcome Fletch.

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -30,6 +30,7 @@ public class GameManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
     }
 }
