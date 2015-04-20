b223a6860d95ee77983e08d210f16579fd6d8110
Pal Mezei <sirpalee@gmail.com>
2015-04-21 02:47:26 +1000

    Splatting.

---
+++ Assets/Code/KillAnimatedSprite.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillAnimatedSprite : MonoBehaviour {
+
+    public AnimationClip targetAnim;
+
+    private IEnumerator KillOnAnimationEnd()
+    {
+        yield return new WaitForSeconds (targetAnim.length - 0.05f);
+        Destroy (gameObject);
+    }
+    
+    void Start()
+    {
+        StartCoroutine (KillOnAnimationEnd());
+    }
+}
